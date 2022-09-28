baae20891f7b640eba806dab37764a263379936a
rachelcassway <105314113+rachelcassway@users.noreply.github.com>
2022-09-27 21:56:55 -0400

    buttons work

---
+++ main.js
@@ -0,0 +1,13 @@
+function button1Clicked() {
+	let text = "Paragraph 1"
+
+	let button1Div = document.getElementById("button1-div")
+	button1Div.innerHTML = text
+}
+
+function button2Clicked() {
+	let text = "Paragraph 2"
+
+	let button2Div = document.getElementById("button2-div")
+	button2Div.innerHTML = text
+}
