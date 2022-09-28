4c995ed93ecf3ae3cb660ba59bbab572885e3309
rachelcassway <105314113+rachelcassway@users.noreply.github.com>
2022-09-28 15:12:40 -0400

    new organization

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-function button1Clicked() {
+function v1button1Clicked() {
 	let text = "Paragraph 1"
 
-	let button1Div = document.getElementById("button1-div")
-	button1Div.innerHTML = text
+	let v1button1Div = document.getElementById("vis1button1")
+	v1button1Div.innerHTML = text
 }
 
-function button2Clicked() {
+function v1button2Clicked() {
 	let text = "Paragraph 2"
 
-	let button2Div = document.getElementById("button2-div")
-	button2Div.innerHTML = text
+	let v1button2Div = document.getElementById("vis1button2")
+	v1button2Div.innerHTML = text
+}
+
+function v2button1Clicked() {
+	let text = "Paragraph 1"
+
+	let v2button1Div = document.getElementById("vis2button1")
+	v2button1Div.innerHTML = text
+}
+
+function v2button2Clicked() {
+	let text = "Paragraph 2"
+
+	let v2button2Div = document.getElementById("vis2button2")
+	v2button2Div.innerHTML = text
 }
